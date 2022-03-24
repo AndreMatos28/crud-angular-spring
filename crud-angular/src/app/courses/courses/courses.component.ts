fa88c0de36c50793396a5c26fb55639ea31a8b7f
AndreMatos28 <andrealleati@gmail.com>
2022-03-23 21:37:33 -0300

    course service

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -7,12 +8,17 @@ import { Course } from '../model/course';
   styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
-  courses: Course[] = [
-    { _id: "1", name: 'Angular', category: 'Java'}
-  ];
+  
+  courses: Course[] = [];
   displayedColumns = ['name', 'category'];
 
-  constructor() {}
+  //coursesService: CoursesService;
+
+  constructor(private coursesService: CoursesService) {
+   //this.coursesService =  new CoursesService();
+
+    this.courses = this.coursesService.list();
+  }
 
   ngOnInit(): void {}
 }
