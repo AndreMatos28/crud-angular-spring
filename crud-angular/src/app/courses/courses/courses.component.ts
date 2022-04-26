e5fb07625558fc94512d6aab300623b1ed2ad4c5
AndreMatos28 <andrealleati@gmail.com>
2022-04-25 22:13:52 -0300

    front angular

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,25 +1,57 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { ErrorDialogComponent } from '../../shared/components/error-dialog/error-dialog.component';
 import { Course } from '../model/course';
-import { CoursesService } from '../services/courses.service';
+import { CoursesService } from './../services/courses.service';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss'],
+  styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
-  
-  courses: Observable<Course[]>;
-  displayedColumns = ['name', 'category'];
 
-  //coursesService: CoursesService;
+  courses$: Observable<Course[]>;
+  // courses: Course[] = [];
+  displayedColumns = ['name','category','actions'];
 
-  constructor(private coursesService: CoursesService) {
-   //this.coursesService =  new CoursesService();
+  // coursesService: CoursesService;
 
-    this.courses = this.coursesService.list();
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    // this.courses = [];
+    // this.coursesService = new CoursesService();
+    this.courses$ = this.coursesService.list()
+    .pipe(
+      catchError(error => {
+        this.onError('Erro ao carregar cursos.');
+        return of([])
+      })
+    );
+
+    // this.coursesService.list().subscribe(courses => this.courses = courses);
   }
 
-  ngOnInit(): void {}
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  onAdd() {
+    this.router.navigate(['new'], {relativeTo: this.route});
+  }
+
 }
